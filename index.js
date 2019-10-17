c8e1cbd03604ee118e4fc33287e46f59990104a8
Abel Perez <Abel.Perez@ao.com>
2019-10-17 17:01:24 +0100

    Added some http request code

--- index.js
+++ index.js
@@ -1,31 +1,114 @@
 const yargs = require('yargs');
 const reader = require("readline-sync");
 const fs = require('fs');
+const os = require('os');
+const path = require('path')
+const https = require('http')
+//const https = require('https')
 
 const argv = yargs
-    .command('login', 'Ask for credentials to authenticate')
+	.command('login', 'Ask for credentials to authenticate')
 	.command('exec', 'Execute command')
-    .help()
-    .argv;
+	.help()
+	.argv;
+
+const filename = path.join(os.tmpdir(), 'token');
+//console.log(filename);
 
 if (argv._.includes('login')) {
 
 
 	const username = reader.question("Username: ");
 	const password = reader.question("Password: ", { hideEchoBack: true });
-	console.log(`You entered: ${username} & ${password}`);
 
+	const data = JSON.stringify({
+		Email: username,
+		Password: password
+	});
 
-	console.log('writing it to a file');
-	fs.writeFileSync('file.txt', `You entered: ${username} & ${password}`);
-	console.log('done');
+	const options = {
+		hostname: 'localhost',
+		port: 60000,
+		path: '/api/token/',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': data.length
+		}
+	};
 
+	const req = https.request(options, res => {
+
+		if (res.statusCode == 200) {
+			console.log('Authentication successful');
+		} else {
+			console.log('Authentication failed');
+		}
+
+		res.on('data', d => {
+			fs.writeFileSync(filename, d);
+			console.log('Token saved');
+		})
+	})
+
+	req.on('error', error => {
+		console.error(error)
+	})
+
+	req.write(data)
+	req.end()
+
+}
+else
+	if (argv._.includes('exec')) {
+		console.log('exec command');
+
+		if (!fs.existsSync(filename)){
+			console.log('No authentication token found, please use login command');
+			process.exit(0);
+		}
+
+		const token = fs.readFileSync(filename);
+
+		console.log('Token found');
+
+		const data = JSON.stringify({
+		});
 	
-}
-else 
-if (argv._.includes('exec')) {
-    console.log('exec command');
-}
-else {
-	console.log('Unknown command: ', argv._);
-}
+		const options = {
+			hostname: 'localhost',
+			port: 60000,
+			path: '/api/dynamocode',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Content-Length': data.length,
+				'Authorization': 'Bearer ' + token.toString()
+			}
+		};
+
+		const req = https.request(options, res => {
+
+			if (res.statusCode == 200) {
+				console.log('exec request successful');
+			} else {
+				console.log('exec request failed');
+			}
+	
+			res.on('data', d => {
+				
+				console.log(d.toString());
+			})
+		})
+	
+		req.on('error', error => {
+			console.error(error)
+		})
+	
+		req.write(data)
+		req.end()
+
+	}
+	else {
+		console.log('Unknown command: ', argv._);
+	}
