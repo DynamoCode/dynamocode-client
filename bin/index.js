31b3dd223926fcdd20c8790347098364050d61bd
Abel Perez <Abel.Perez@ao.com>
2019-10-31 16:29:04 +0000

    Added configuration

--- bin/index.js
+++ bin/index.js
@@ -4,15 +4,17 @@ const reader = require("readline-sync");
 const cmdline = require('../lib/cmdline')
 const login = require('../lib/login')
 const exec = require('../lib/exec')
+const config = require('../lib/config')
 
 const argv = cmdline.argv;
+const cfg = config.getConfig(argv);
 
 if (argv._.includes('login')) {
 
     const username = reader.question("Username: ");
     const password = reader.question("Password: ", { hideEchoBack: true });
 
-    login.Login(username, password);
+    login.Login(cfg, username, password);
 }
 else
     if (argv._.includes('exec')) {
@@ -24,7 +26,7 @@ else
         }
 
         const token = login.getToken();
-        exec.executeCommand(token, argv.template, argv.entity);
+        exec.executeCommand(cfg, token, argv.template, argv.entity);
     }
     else {
         console.log('Unknown command: ', argv._);
